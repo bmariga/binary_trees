352d83ef6331e927c1bc35f303b0e880d9133e81
Mariga Bruce <mariga.bn@gmail.com>
2023-08-31 13:45:06 +0300

    measures the size of a binary tree

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -3,7 +3,7 @@
 /**
  * binary_tree_size - measures the size of a binary tree
  * @tree: pointer to the root node
- * Return: 0 if tree is NULL
+ * Return: if tree is NULL, return 0
  */
 
 size_t binary_tree_size(const binary_tree_t *tree)
